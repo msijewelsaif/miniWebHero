b7d4aa4cd859e68e5966b0108058a7cec20134dd
Md Saiful Islam <94510295+msijewelsaif@users.noreply.github.com>
2024-09-16 01:01:06 +0600

    Add React App

---
+++ TicTacReact/tic-tac-toe/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Board from './Board';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Tic Tac Toe</h1>
+        <Board />
+      </header>
+    </div>
+  );
+}
+
+export default App;
